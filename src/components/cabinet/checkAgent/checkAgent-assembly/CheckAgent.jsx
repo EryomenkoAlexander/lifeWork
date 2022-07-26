5e4726de16826d971f8bfc24daa8aac1115bd937
EryomenkoAlexander <sasha.eryomenko77@gmail.com>
2022-07-26 21:32:43 +0600

    create check agent

--- src/components/cabinet/checkAgent/checkAgent-assembly/CheckAgent.jsx
+++ src/components/cabinet/checkAgent/checkAgent-assembly/CheckAgent.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { TextField } from '@mui/material'
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import s from './CheckAgent.module.css'
+import SearchIcon from '@mui/icons-material/Search';
+import { setShowPopup } from '../../../../redux/slices/successPopup-slice'
 
 const CheckAgent = () => {
+  let dispatch = useDispatch()
+  let checkAgent = useSelector(state => state.cabinet.checkAgent)
+  let [value, setValue] = useState('')
+  
+  let sendAgent = () => {
+    if (!value.includes(' ') && value !== '') {
+      dispatch(setShowPopup(true))
+      setTimeout(() => {
+        dispatch(setShowPopup(false))
+      }, 2500)
+      setValue('')
+    }
+  }
+  
   return (
-    <h1>CheckAgent</h1>
-  )
+    <div className={s.checkAgent}>
+      <h2>{checkAgent.title}</h2>
+      <div className={s.wrapper}>
+        <div className={s.search}>
+          <TextField
+            label="ИНН / БИН / ОГРН"
+            variant="standard"
+            sx={{ width: "100%" }}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <SearchIcon className={s.icon} fontSize='large' color='primary' onClick={sendAgent} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CheckAgent
