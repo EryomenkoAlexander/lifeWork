8e6948332aa885219accf9058d9634c816bcd2ef
EryomenkoAlexander <sasha.eryomenko77@gmail.com>
2022-07-29 14:27:01 +0600

    refactoring list header (help, location)

--- src/components/header/nav/nav-item/NavItem.jsx
+++ src/components/header/nav/nav-item/NavItem.jsx
@@ -1,55 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import NavItemList from "../nav-item-list/NavItemList";
-import s from './NavItem.module.css'
+import s from "./NavItem.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { setShowList } from "../../../../redux/slices/header-slice";
-import { setOpen } from "../../../../redux/slices/myAlert-slice";
 
 const NavItem = (props) => {
-  let dispatch = useDispatch()
+  let dispatch = useDispatch();
 
-  let isLoggedIn = useSelector(state => state.user.isLoggedIn)
-  let location = useSelector(state => state.user.activeUser.location || 'Город')
-
-  let showList = (e) => {
-    if (props.data.typeList === 'location') {
-      if (isLoggedIn) {
-        dispatch(setShowList(props.data.typeList))
-      } else {
-        dispatch(setOpen({
-          type: 'warning',
-          text: 'Выполните вход'
-        }))
-      }
-    } else {
-      dispatch(setShowList(props.data.typeList))
-    }
-  }
+  let location = useSelector(
+    (state) => state.user.activeUser.location || "Город"
+  );
 
   let elNavItem;
+
   if (props.data.list) {
     elNavItem = (
-      <div className={s.navItemList} onClick={() => showList()} key={props.data.id}>
-        <span className={props.data.typeList === 'location' ? s.location : ''}>{props.data.typeList === 'location' ? location : props.data.title}</span>
+      <div
+        className={s.navItemList}
+        onClick={() => dispatch(setShowList(props.data.typeList))}
+        key={props.data.id}
+      >
+        <span className={props.data.typeList === "location" ? s.location : ""}>
+          {props.data.typeList === "location" ? location : props.data.title}
+        </span>
         <img src={props.data.arrowImg} alt="arrow" />
         <div className={props.data.isShowList ? s.active : s.inactive}>
-          <NavItemList
-            list={props.data.list}
-            typeList={props.data.typeList}
-          />
+          <NavItemList list={props.data.list} typeList={props.data.typeList} />
         </div>
       </div>
     );
   } else {
-    elNavItem = <Link to={props.data.to} key={props.data.id}>{props.data.title}</Link>;
+    elNavItem = (
+      <Link to={props.data.to} key={props.data.id}>
+        {props.data.title}
+      </Link>
+    );
   }
 
-    return (
-      <div className={s.navItem}>
-        {elNavItem}
-      </div>
-    );
-}
+  return <div className={s.navItem}>{elNavItem}</div>;
+};
 
 export default NavItem;
